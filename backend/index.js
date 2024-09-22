1766b7608a949938e8ae03347e43e71e2c0cda49
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-09-22 00:02:14 -0700

    db and app models created

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import dotenv from "dotenv";
-
+import connectDB from "./utils/db.js";
 dotenv.config({});
 const app = express();
 
@@ -20,5 +20,6 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3900;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port ${PORT}`);
 });
